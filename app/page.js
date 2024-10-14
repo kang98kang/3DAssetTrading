d40ddea634cc4ec8b918707e99ffa820b69fb5db
KANG <skystar0071@naver.com>
2024-10-14 17:01:04 +0900

    add components/common/button

--- app/page.js
+++ app/page.js
@@ -1,5 +1,31 @@
+"use client";
+
 import styles from "./mainpage.module.css";
+import Button from "../components/common/button";
 
 export default function Home() {
-  return <div className={styles.dummy}>dummy main body :) </div>;
+  return (
+    <div className={styles.dummy}>
+      <p>dummy main body :)</p>
+      <br />
+      <p style={{ color: "green" }}>Button Test</p>
+      <div style={{ display: "flex", gap: 20 }}>
+        <Button
+          backgroundColor="red"
+          label="Red"
+          onClick={() => alert("RED TEST!")}
+        />
+        <Button
+          backgroundColor="green"
+          label="Green"
+          onClick={() => alert("Green TEST!")}
+        />
+        <Button
+          backgroundColor="blue"
+          label="Blue"
+          onClick={() => alert("Blue TEST!")}
+        />
+      </div>
+    </div>
+  );
 }
