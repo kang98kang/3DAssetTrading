0e74b05cf2296e065a333a04147ff0a09a8fd64e
Hyeju Jeong <hyeju518@gmail.com>
2024-10-14 16:57:23 +0900

    add a dropdown list when hovered

--- app/Header.js
+++ app/Header.js
@@ -1,39 +1,20 @@
-"use client";
-
-import { useState } from "react";
 import styles from "./Header.module.css";
+import Explore from "./Explore";
 
 export default function Header() {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownVisible(!isDropdownVisible);
-  };
-
   return (
     <>
       <div className={styles.header}>
         <div className={styles.logoContainer}>
           <div className={styles.logo}>Logo</div>
           <div className={`${styles.logo} ${styles.name}`}>Name</div>
-          <div className={styles.explore} onMouseEnter={toggleDropdown}>
-            Explore🔽
-          </div>
+          <Explore />
         </div>
         <input type="text" placeholder="SearchBar" className={styles.nav} />
         <div className={styles.loginContainer}>
           <div className={styles.login}>Login</div>
           <div className={styles.login}>Signin</div>
         </div>
-        {/* {isDropdownVisible && (
-          <ul className={styles.dropdown}>
-            <li>Extension</li>
-            <li>Rendering</li>
-            <li>Modeling</li>
-            <li>Price</li>
-            <li>Animated</li>
-          </ul>
-        )} */}
       </div>
     </>
   );
