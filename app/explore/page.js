ca7ca02892485ba673c78dcbccb6c86d25c017e5
Hyeju Jeong <hyeju518@gmail.com>
2024-10-16 15:00:09 +0900

    Map explore menus

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,16 +1,18 @@
 import styles from "./explorepage.module.css";
 
 export default function Explore() {
+  const filters = ["EXTENSION", "RENDERING", "MODELING", "PRICE", "ANIMATION"];
   return (
     <div className={styles.dummy}>
       <div className={styles.divisionline}></div>
       <div className={styles.nav}>
-        <div className={styles.filter1}>EXTENSION</div>
-        <div className={styles.filter2}>RENDERING</div>
-        <div className={styles.filter3}>MODELING</div>
-        <div className={styles.filter4}>PRICE</div>
-        <div className={styles.filter5}>ANIMATION</div>
+        {filters.map((filter, index) => (
+          <div key={index} className={styles.filter}>
+            {filter}
+          </div>
+        ))}
       </div>
+      <div style={{ backgroundColor: "red" }}>red</div>
     </div>
   );
 }
