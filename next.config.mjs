b5cb4736ef594082fed732084a7994b7cf94792e
Hyeju Jeong <hyeju518@gmail.com>
2024-10-22 16:19:09 +0900

    fix: removed logging and edited next.config.mjs to use NEXT_PUBLIC environment variables through instead of using .env file directly

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // 빌드 중 ESLint 경고 무시
+  env: {
+    NEXT_PUBLIC_BACKGROUND_COLOR_PRIMARY: "#808080",
+    NEXT_PUBLIC_BACKGROUND_COLOR_SECONDARY: "#2b2b2b",
+    NEXT_PUBLIC_BACKGROUND_COLOR_TERTIARY: "#3a3a3a",
+    NEXT_PUBLIC_ACCENT_COLOR_PRIMARY: "#97bdba",
+    NEXT_PUBLIC_TEXT_COLOR_PRIMARY: "white",
+    NEXT_PUBLIC_TEXT_COLOR_SECONDARY: "black",
   },
 };
 
